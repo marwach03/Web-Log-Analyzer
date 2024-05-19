68b6d516c8c4f91b8d0030ac1d6f00f457b85819
unknown <ikram.belhaj2003@gmail.com>
2024-05-19 17:40:51 +0100

    La partie HTML ,script

---
+++ Project wla/static/js/script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var menuIcon = document.getElementById("menu-icon");
+    var menu = document.getElementById("menu");
+
+    menuIcon.addEventListener("click", function() {
+        menu.classList.toggle("show");
+    });
+});
